a6dcbd2a687f3becfa9b0a5e03e3a089d35689a9
lawmaso <masonlaw.cs@gmail.com>
2026-08-17 23:02:29 -0400

    fix: init on install and startup

--- background.js
+++ background.js
@@ -5,14 +5,17 @@ async function broadcastSashimiState(on = true, text = CONFIG.ON) {
     chrome.action.setBadgeText({ text });
 }
 
-chrome.runtime.onInstalled.addListener(async () => {
+async function initSashimi() {
     const state = await chrome.storage.local.get(["on"]);
     if (state.on === undefined) {
         await broadcastSashimiState();
     } else {
         await broadcastSashimiState(state.on, state.on ? CONFIG.ON : CONFIG.OFF);
     }
-});
+}
+
+chrome.runtime.onInstalled.addListener(initSashimi);
+chrome.runtime.onStartup.addListener(initSashimi);
 
 chrome.action.onClicked.addListener(async (tab) => {
     const state = await chrome.storage.local.get(["on"]);
