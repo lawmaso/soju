03ad6a5a0b789b3fb60d519a6dd98a2394342fc8
lawmaso <masonlaw.cs@gmail.com>
2026-08-17 22:14:00 -0400

    refactor: add config file

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    NAME:          "Sashimi - AI Overview Toggle",
+    DESCRIPTION:   "A simple UI toggle",
+    VERSION:       "1.0",
+    DEFAULT_TITLE: "Toggle Sashimi",
+    MATCHES:       ["https://*.google.com/*"],
+    SASHIMI_ID:    "_sashimi",
+    ON:            "ON",
+    OFF:           "OFF"
+};
+
+export default CONFIG;
